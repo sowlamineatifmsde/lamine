68d00e652ab107cb397668cb07744fdb60ccfe7d
sowlamineatifmsde <112579306+sowlamineatifmsde@users.noreply.github.com>
2022-09-03 22:03:00 +0100

    Update Modele_Final.py

--- Modele_Final.py
+++ Modele_Final.py
@@ -16,10 +16,7 @@ option = st.selectbox(
 
 
 if option=='input parameters directly':
-    uploaded_file = st.file_uploader("Choose a file to load")
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
-        st.write(df)
+     st.balloons()
 
 
 
